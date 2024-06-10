418746289fe59c1c01b70b6643ef376ffe8eaa43
Kimbali <kimgarcianton@hotmail.com>
2024-06-10 18:48:33 +0200

    geolocate user on the map

--- app/routes/temperature.tsx
+++ app/routes/temperature.tsx
@@ -1,13 +1,15 @@
-import { LoaderFunction, json } from '@remix-run/node';
-import { useState } from 'react';
+import { ActionFunctionArgs, LoaderFunctionArgs, json } from '@remix-run/node';
+import { useEffect, useState } from 'react';
 import CurrentTemperature from '~/components/temperature/CurrentTemperature';
 import ForecastComponent from '~/components/temperature/Forecast';
 import TrendComponent from '~/components/temperature/Trend';
-import { Title } from '~/components/text';
+import { Text, Title } from '~/components/text';
 import { Tomorrow } from '~/services/api/tomorrow';
 import { ClientOnly } from 'remix-utils/client-only';
 import type { LatLngExpression } from 'leaflet';
 import { Map } from '~/components/map/map.client';
+import { useFetcher, useLoaderData } from '@remix-run/react';
+import { commitSession, getSession } from '~/services/session.server';
 
 export interface OneDay {
   day: string;
@@ -25,15 +27,19 @@ export interface TemperatureDate {
 
 const tomorrowCtrl = new Tomorrow();
 
-export const loader: LoaderFunction = async () => {
+export async function loader({ request }: LoaderFunctionArgs) {
+  const session = await getSession(request.headers.get('cookie'));
+  const latitude = session.get('latitude');
+  const longitude = session.get('longitude');
+
   const resForecast = await tomorrowCtrl.getForecast({
     lat: '42.3478',
     long: '-71.0466',
   });
 
   const resTrend = await tomorrowCtrl.getTrend({
-    lat: '42.3478',
-    long: '-71.0466',
+    lat: latitude || '42.3478',
+    long: longitude || '-71.0466',
   });
 
   if (resForecast.error || resTrend.error) {
@@ -43,27 +49,56 @@ export const loader: LoaderFunction = async () => {
     );
   }
 
-  return {
+  return json({
     currentTemperature: resForecast.currentTemperature,
     forecast: resForecast.forecast,
     trend: resTrend,
-  };
-};
+    latitude,
+    longitude,
+  });
+}
+
+export async function action({ request }: ActionFunctionArgs) {
+  const coords = await request.json();
+  const session = await getSession(request.headers.get('cookie'));
+  session.set('latitude', coords.latitude);
+  session.set('longitude', coords.longitude);
+
+  return json(
+    { success: true },
+    { headers: { 'set-cookie': await commitSession(session) } }
+  );
+}
 
 export default function TemperaturePage() {
   const mapHeight = '400px';
+  const { latitude, longitude } = useLoaderData<typeof loader>();
+
   const [selectedLocation, setSelectedLocation] = useState<LatLngExpression>({
-    lat: 41.38694691885317,
-    lng: 2.1676698133663157,
+    lat: latitude || 41.38694691885317,
+    lng: longitude || 2.1676698133663157,
   });
+
+  const updatePosition = ({ latitude, longitude }) => {
+    setSelectedLocation({ lat: latitude, lng: longitude });
+    fetcher.submit(
+      { latitude, longitude },
+      { method: 'post', encType: 'application/json' }
+    );
+  };
 
   const handleSelect = (location: LatLngExpression) => {
     const { lat, lng } = location;
-
-    const coords = { lat, lng };
-
-    setSelectedLocation(coords);
+    updatePosition({ latitude: lat, longitude: lng });
   };
+
+  const fetcher = useFetcher();
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(position => {
+      const { latitude, longitude } = position.coords;
+      updatePosition({ latitude, longitude });
+    });
+  }, []);
 
   return (
     <div className='pb-16 w-full'>
@@ -91,6 +126,10 @@ export default function TemperaturePage() {
           />
         )}
       </ClientOnly>
+
+      <Text>
+        <span>Lat: {latitude}</span>, Lng: <span>{longitude}</span>
+      </Text>
     </div>
   );
 }
